3798308103a371f7387425bd5c9b03c35001d89d
tomonobu <tomonobu.sasaki@bravi.co.jp>
2017-10-13 08:41:12 +0000

    set the correct path

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,7 +10,7 @@
 | and give it the controller to call when that URI is requested.
 |
 */
-Route::get('tasks', 'TasksController@index');
+Route::get('/', 'TasksController@index');
 Route::resource('tasks', 'TasksController');
 /*
 Route::get('/', function () {
